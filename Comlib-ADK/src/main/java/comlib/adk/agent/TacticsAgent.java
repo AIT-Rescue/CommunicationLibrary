3a8391f532d112ec866d94d7fafaf69f587026cb
uranos <uranoskoro@gmail.com>
2014-10-02 18:59:11 +0900

    add src

--- Comlib-ADK/src/main/java/comlib/adk/agent/TacticsAgent.java
+++ Comlib-ADK/src/main/java/comlib/adk/agent/TacticsAgent.java
@@ -2,18 +2,37 @@ package comlib.adk.agent;
 
 import comlib.adk.tactics.Tactics;
 import comlib.agent.CommunicationAgent;
+import comlib.agent.MessageEvent;
 import rescuecore2.standard.entities.StandardEntity;
+import rescuecore2.worldmodel.ChangeSet;
 
 public abstract class TacticsAgent<E extends StandardEntity> extends CommunicationAgent<E>
 {
     public Tactics tactics;
 
-    private TacticsAgent() {
+    public TacticsAgent(Tactics t){
         super();
+        this.tactics = t;
     }
 
-    public TacticsAgent(Tactics t){
-        this();
-        this.tactics = t;
+    /*@Override
+    protected EnumSet<StandardEntityURN> getRequestedEntityURNsEnum() {
+        return null;
+    }*/
+
+    @Override
+    public MessageEvent getMessageEvent() {
+        return null;
+    }
+
+    @Override
+    public void postConnect() {
+        super.postConnect();
+        this.tactics.postConnect();
+    }
+
+    @Override
+    public void think(int time, ChangeSet changed) {
+        this.send(this.tactics.think());
     }
 }
