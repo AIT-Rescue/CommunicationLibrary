1b6c3bcf83206e0778a25ddb4c99438d130b8d50
URANOS1260 <uranoskoro@gmail.com>
2014-10-02 21:00:17 +0900

    remake CommunicationAgent (test)

--- Comlib/src/main/java/comlib/agent/CommunicationAgent.java
+++ Comlib/src/main/java/comlib/agent/CommunicationAgent.java
@@ -13,61 +13,63 @@ import java.util.List;
 
 public abstract class CommunicationAgent<E extends StandardEntity> extends StandardAgent<E>
 {
-	protected MessageManager manager;
-	
-	public CommunicationAgent()
-	{
-		super();
-	}
-	
-	public abstract MessageEvent getMessageEvent();
-	
-	public abstract void think(int time, ChangeSet changed);
-	
-	public void sendSpeak(CommunicationMessage msg)
-	{
-		this.manager.addMessage(msg);
-	}
-	
-	@Override
-	public void postConnect()
-	{
-		super.postConnect();
-		this.manager = new MessageManager(this.config, this.getMessageEvent());
-	}
-	
-	@Override
-	protected final void think(int time, ChangeSet changed, Collection<Command> heard)
-	{
-		this.manager.receiveMessage(time, heard);
-		
-		this.think(time, changed);
-		
-		//this.sendMessage(time);
-		this.send(this.manager.createSendMessage());
-	}
-	
-	public void send(Message[] msgs)
-	{
-		for(Message msg : msgs)
-			this.send(msg);
-	}
-	
-	public void send(List<Message> msgs)
-	{
-		for(Message msg : msgs)
-			this.send(msg);
-	}
-	
-	@Override
-    protected void sendSpeak(int time, int channel, byte[] data)
-	{
-		//super.sendSpeak(time, channel, data);
-	}
-	
-	@Override
-    protected void sendSay(int time, byte[] data)
-	{
-		//super.sendSay(time, data);
-	}
+    public MessageManager manager;
+
+    public CommunicationAgent(){
+        super();
+    }
+
+    public abstract void think(int time, ChangeSet changed);
+
+    public void sendSpeak(CommunicationMessage msg)
+    {
+        this.manager.addMessage(msg);
+    }
+
+    @Override
+    public void postConnect()
+    {
+        super.postConnect();
+        this.manager = new MessageManager(this.config);
+        this.registerCreator();
+        this.registerReceiveEvent();
+    }
+
+    public void registerCreator() {
+    }
+
+    public void registerReceiveEvent() {
+    }
+    //public abstract void registerReceiveEvent();
+
+    @Override
+    protected final void think(int time, ChangeSet changed, Collection<Command> heard)
+    {
+        this.manager.receiveMessage(time, heard);
+
+        this.think(time, changed);
+
+        //this.sendMessage(time);
+        this.send(this.manager.createSendMessage());
+    }
+
+    public void send(Message[] msgs) {
+        for(Message msg : msgs)
+            this.send(msg);
+    }
+
+    public void send(List<Message> msgs) {
+        for(Message msg : msgs)
+            this.send(msg);
+    }
+
+    @Override
+    protected void sendSpeak(int time, int channel, byte[] data) {
+        //super.sendSpeak(time, channel, data);
+    }
+
+    @Override
+    protected void sendSay(int time, byte[] data) {
+        //super.sendSay(time, data);
+    }
 }
