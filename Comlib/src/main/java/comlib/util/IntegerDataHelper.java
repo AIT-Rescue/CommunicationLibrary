457fdf9c7ded623109c106980ddb7a3daff27c54
uranos <uranoskoro@gmail.com>
2014-10-04 00:37:36 +0900

    remake Comlib

--- Comlib/src/main/java/comlib/util/IntegerDataHelper.java
+++ Comlib/src/main/java/comlib/util/IntegerDataHelper.java
@@ -118,20 +118,6 @@ public class IntegerDataHelper
     }
 
     /*public static int getBitSize(int value) {
-        for(int i = 31; i > 0; i--)
-            if(((value >> i) & 0x01) == 1)
-                return i + 1;
-        return 1;
-    }*/
-
-    /*public static int getBitSize(int value) {
-        for(int i = (value >> 16) != 0 ? 31 : 15; i > 0; i--)
-            if(((value >> i) & 0x01) == 1)
-                return i + 1;
-        return 1;
-    }*/
-
-    public static int getBitSize(int value) {
         int i = (value >> 16) != 0 ? (value >> 24) != 0 ? 31 : 23 : (value & 0xFF00) != 0 ? 15 : 7;
         do {
             if (((value >> i) & 0x01) == 1)
@@ -139,5 +125,61 @@ public class IntegerDataHelper
             i--;
         } while (i > 0);
         return 1;
+    }*/
+
+    /*public static int getBitSize(int value) {
+        int i;
+        if((value >> 16) != 0)
+            i = (value >> 24) != 0 ? (value >> 28) != 0 ? 31 : 27 : (value >> 0x00F00000) != 0 ? 23 : 19;
+        else
+            i = (value & 0xFF00) != 0 ? (value & 0xF000) != 0 ? 15 : 11 : (value >> 0xF0) != 0 ? 7 : 3;
+        do {
+            if (((value >> i) & 0x01) == 1)
+                return i + 1;
+            i--;
+        } while (i > 0);
+        return 1;
+    }*/
+
+    public static int getBitSizeOld(int value) {
+        for(int i = (value >> 16) != 0 ? 31 : 15; i > 0; i--)
+            if(((value >> i) & 0x01) == 1)
+                return i + 1;
+        return 1;
+    }
+
+    public static int getBitSize(int value) {
+        if((value & 0xFFFF0000) != 0) {
+            if ((value & 0xFF000000) != 0) {
+                if ((value & 0xF0000000) != 0) {
+                    return (value & 0xC0000000) != 0 ? (value & 0x80000000) != 0 ? 32 : 31 : (value & 0x20000000) != 0 ? 30 : 29;
+                } else {
+                    return (value & 0x0C000000) != 0 ? (value & 0x08000000) != 0 ? 28 : 27 : (value & 0x02000000) != 0 ? 26 : 25;
+                }
+            }
+            else {
+                if ((value & 0x00F00000) != 0) {
+                    return (value & 0x00C00000) != 0 ? (value & 0x00800000) != 0 ? 24 : 23 : (value & 0x00200000) != 0 ? 22 : 21;
+                } else {
+                    return (value & 0x000C0000) == 0 ? (value & 0x00020000) != 0 ? 18 : 17 : (value & 0x00080000) != 0 ? 20 : 19;
+                }
+            }
+        }
+        else { //0x0000FFFF
+            if ((value & 0x0000FF00) != 0) {
+                if ((value & 0x0000F000) != 0) {
+                    return (value & 0x0000C000) != 0 ? (value & 0x00008000) != 0 ? 16 : 15 : (value & 0x00002000) != 0 ? 14 : 13;
+                } else { //0x0F000000
+                    return (value & 0x00000C00) != 0 ? (value & 0x00000800) != 0 ? 12 : 11 : (value & 0x00000200) != 0 ? 10 : 9;
+                }
+            }
+            else { //00FF0000
+                if ((value & 0x000000F0) != 0) {
+                    return (value & 0x000000C0) != 0 ? (value & 0x00000080) != 0 ? 8 : 7 : (value & 0x00000020) != 0 ? 6 : 5;
+                } else { //0x000F0000
+                    return (value & 0x0000000C) != 0 ? (value & 0x00000008) != 0 ? 4 : 3 : (value & 0x00000002) != 0 ? 2 : 1;
+                }
+            }
+        }
     }
 }
