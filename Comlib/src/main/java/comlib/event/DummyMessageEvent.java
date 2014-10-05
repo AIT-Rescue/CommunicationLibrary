a2ba74ce911eec2819e67769a06fc00a28ad1c35
Shunki TAKAMI <tkmnet@users.noreply.github.com>
2014-10-05 21:40:31 +0900

    edited DummyMessageEvent

---
+++ Comlib/src/main/java/comlib/event/DummyMessageEvent.java
@@ -0,0 +1,14 @@
+package comlib.event;
+
+
+public abstract class DummyMessageEvent<DummyMessage> {
+
+	public void receivedRadio(M msg)
+	{
+	}
+
+	public void receivedVoice(M msg)
+	{
+		this.receivedRadio();
+	}
+}
