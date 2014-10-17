9ea9bae930549ca1235f64ea0031c1c5b52e0dbc
Shunki TAKAMI <tkmnet@users.noreply.github.com>
2014-10-17 18:08:46 +0900

    update Comlib

--- Comlib/src/main/java/comlib/message/information/VictimMessage.java
+++ Comlib/src/main/java/comlib/message/information/VictimMessage.java
@@ -7,41 +7,14 @@ import rescuecore2.worldmodel.EntityID;
 
 abstract public class VictimMessage extends HumanMessage
 {
-	protected EntityID humanID;
-	protected int humanHP;
-	protected int humanBuriedness;
-	protected int humanDamage;
-	protected EntityID humanPosition;
-
-	public VictimMessage(int messageID, Human human)
+	public VictimMessage(Human human)
 	{
-		super(messageID);
-		humanID = human.getID();
-		humanHP = human.getHP();
-		humanBuriedness = human.getBuriedness();
-		humanDamage = human.getDamage();
-		humanPosition = human.getPosition();
+		super(MessageID.victimMessage, human);
 	}
 
-	public VictimMessage(int messageID, int time, int ttl, EntityID id, int hp, int buriedness, int damage, EntityID position)
+	public VictimMessage(int time, int ttl, EntityID id, int hp, int buriedness, int damage, EntityID position)
 	{
-		super(messageID, time, ttl);
-		humanID = id;
-		humanHP = hp;
-		humanBuriedness = buriedness;
-		humanDamage = damage;
-		humanPosition = position;
+		super(MessageID.victimMessage, int time, int ttl, EntityID id, int hp, int buriedness, int damage, EntityID position)
 	}
-
-	public EntityID getHumanID() { return this.humanID; }
-
-	public int getHP() { return this.humanHP; }
-
-	public int getBuriedness() { return this.humanBuriedness; }
-
-	public int getDamage() { return this.humanDamage; }
-
-	public EntityID getPosition() { return this.humanPosition; }
-
 }
 
