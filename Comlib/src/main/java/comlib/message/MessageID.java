9f77d7900d1863f1a5137361ec5d8e0f81a0c82b
Shunki TAKAMI <tkmnet@users.noreply.github.com>
2014-10-17 18:53:50 +0900

    update Comlib

--- Comlib/src/main/java/comlib/message/MessageID.java
+++ Comlib/src/main/java/comlib/message/MessageID.java
@@ -3,9 +3,9 @@ package comlib.message;
 
 public interface MessageID
 {
-	int dummyMessage = 10;
+	int dummyMessage = 15;
 
-	int victimMessage = 0;
+	int civilianMessage = 0;
 	int fireBrigadeMessage = 1;
 	int policeForceMessage = 2;
 	int ambulanceTeamMessage = 3;
--- Comlib/src/main/java/comlib/message/information/VictimMessage.java
+++ Comlib/src/main/java/comlib/message/information/VictimMessage.java
@@ -2,21 +2,21 @@ package comlib.message;
 
 import comlib.message.HumanMessage;
 
-import rescuecore2.standard.entities.Victim;
+import rescuecore2.standard.entities.Civilian;
 import rescuecore2.standard.entities.Human;
 import rescuecore2.worldmodel.EntityID;
 
 
-abstract public class VictimMessage extends HumanMessage
+abstract public class CivilianMessage extends HumanMessage
 {
-	public VictimMessage(Victim victim)
+	public CivilianMessage(Civilian civilian)
 	{
-		super(MessageID.victimMessage, (Human)victim);
+		super(MessageID.civilianMessage, (Human)civilian);
 	}
 
-	public VictimMessage(int time, int ttl, EntityID id, int hp, int buriedness, int damage, EntityID position)
+	public CivilianMessage(int time, int ttl, EntityID id, int hp, int buriedness, int damage, EntityID position)
 	{
-		super(MessageID.victimMessage, time, ttl, id, hp, buriedness, damage, position);
+		super(MessageID.civilianMessage, time, ttl, id, hp, buriedness, damage, position);
 	}
 }
 
