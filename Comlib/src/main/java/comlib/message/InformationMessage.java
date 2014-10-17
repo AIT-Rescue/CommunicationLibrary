69e25518a4c01b0b43f9ca0db261c15248650c2f
Shunki TAKAMI(takamin@sylph) <maslab@takamin.net>
2014-10-17 16:27:40 +0900

    add InformationMessage

---
+++ Comlib/src/main/java/comlib/message/InformationMessage.java
@@ -0,0 +1,13 @@
+package comlib.message;
+
+public class InformationMessage extends CommunicationMessage {
+
+	public DummyMessage(int messageID) {
+		super(messageID);
+	}
+
+	public DummyMessage(int messageID, int time, int ttl) {
+		super(messageID, time, ttl);
+	}
+
+}
