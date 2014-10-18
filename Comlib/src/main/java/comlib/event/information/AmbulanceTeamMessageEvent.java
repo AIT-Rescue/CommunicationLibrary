1de1a0b43d1c9c50d22da1f99e47a2f306e21f72
Shunki TAKAMI <tkmnet@users.noreply.github.com>
2014-10-18 20:53:24 +0900

    update Comlib

---
+++ Comlib/src/main/java/comlib/event/information/AmbulanceTeamMessageEvent.java
@@ -0,0 +1,19 @@
+package comlib.event;
+
+import comlib.message.AmbulanceTeamMessage;
+
+
+public abstract class AmbulanceTeamMessageEvent extends MessageEvent<AmbulanceTeamMessage>
+{
+	public void receivedRadio(AmbulanceTeamMessage msg)
+	{
+		// You cannot write here.
+	}
+
+	public void receivedVoice(AmbulanceTeamMessage msg)
+	{
+		// You cannot write here.
+		// This code is default handler.
+		this.receivedRadio(msg);
+	}
+}
