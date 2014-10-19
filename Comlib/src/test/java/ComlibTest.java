69caad0ee38a64fba0afb16a02ef61013c2a97b6
Shunki TAKAMI <tkmnet@users.noreply.github.com>
2014-10-20 02:20:05 +0900

    update Comlib

--- Comlib/src/test/java/ComlibTest.java
+++ Comlib/src/test/java/ComlibTest.java
@@ -1,28 +1,28 @@
-import comlib.manager.MessageManager;
-import rescuecore2.config.Config;
-import junit.framework.TestCase;
-
-
+// import comlib.manager.MessageManager;
+// import rescuecore2.config.Config;
+// import junit.framework.TestCase;
+//
+//
 public class ComlibTest extends TestCase
 {
-	MessageManager manager;
-	Config config;
-
+// 	MessageManager manager;
+// 	Config config;
+//
 	public ComlibTest(String name)
 	{
 		super(name);
 	}
-
-	public void testDummyMessage()
-	{
-		config = new Config();
-		try
-		{
-			config.read("sample.cfg");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		manager = new MessageManager(config);
-		assertEquals(1, 1);
-	}
+//
+// 	public void testDummyMessage()
+// 	{
+// 		config = new Config();
+// 		try
+// 		{
+// 			config.read("sample.cfg");
+// 		} catch (Exception e) {
+// 			e.printStackTrace();
+// 		}
+// 		manager = new MessageManager(config);
+// 		assertEquals(1, 1);
+// 	}
 }
