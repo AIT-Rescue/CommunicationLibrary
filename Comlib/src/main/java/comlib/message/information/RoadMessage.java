7deda8b17510470d189e93841e1bf9dfbe13a8fa
uranos <uranoskoro@gmail.com>
2014-11-03 01:12:05 +0900

    add RoadMessage ()

--- Comlib/src/main/java/comlib/message/information/RoadMessage.java
+++ Comlib/src/main/java/comlib/message/information/RoadMessage.java
@@ -7,40 +7,52 @@ import rescuecore2.worldmodel.EntityID;
 
 public class RoadMessage extends MapMessage {
 
-    private EntityID id;
-    private EntityID position;
+    private int rawBlockadeID;
+    private int rawHumanPosition;
+    private EntityID blockadeID;
+    private EntityID humanPosition;
     private int repairCost;
-    private int[] apexes;
-    private int x;
-    private int y;
+    //private int[] apexes;
+    //private int x;
+    //private int y;
 
     public RoadMessage(Blockade blockade) {
         super(MessageID.roadMessage);
-        this.id = blockade.getID();
-        this.position = blockade.getPosition();
+        this.blockadeID = blockade.getID();
+        this.humanPosition = blockade.getPosition();
         this.repairCost = blockade.getRepairCost();
-        this.apexes = blockade.getApexes();
-        this.x = blockade.getX();
-        this.y = blockade.getY();
+        //this.apexes = blockade.getApexes();
+        //this.x = blockade.getX();
+        //this.y = blockade.getY();
 
     }
 
-    public RoadMessage() {
-        super(MessageID.roadMessage);
+    public RoadMessage(int time, int ttl, int id, int position, int cost) {
+        super(MessageID.roadMessage, time, ttl);
+        this.rawBlockadeID = id;
+        this.rawHumanPosition = position;
+        this.repairCost = cost;
     }
 
-    public EntityID getId() {
-        return this.id;
+    public EntityID getID() {
+        if(this.blockadeID == null) {
+            this.blockadeID = new EntityID(this.rawBlockadeID);
+        }
+        return this.blockadeID;
     }
 
     public EntityID getPosition() {
-        return this.position;
+        if(this.humanPosition == null) {
+            this.humanPosition = new EntityID(this.rawHumanPosition);
+        }
+        return this.humanPosition;
     }
 
     public int getRepairCost() {
         return this.repairCost;
     }
 
+    /*
     public int[] getApexes() {
         return this.apexes;
     }
@@ -52,4 +64,5 @@ public class RoadMessage extends MapMessage {
     public int getY() {
         return this.y;
     }
+    */
 }
