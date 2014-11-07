b9c20417f0fe0bb729a9601e2887f3920c48555c
uranos <uranoskoro@gmail.com>
2014-11-07 17:13:30 +0900

    fix config

--- Comlib-ADK/src/main/java/comlib/adk/launcher/ConfigKey.java
+++ Comlib-ADK/src/main/java/comlib/adk/launcher/ConfigKey.java
@@ -3,12 +3,12 @@ package comlib.adk.launcher;
 public final class ConfigKey {
     public static final String KEY_DIRECTORY = "comlib.adk.launcher.directory";
     
-    public static final String KEY_AMBULANCE_NAME = "comlib.adk.agent.ambulance.name";
-    public static final String KEY_AMBULANCE_COUNT = "comlib.adk.agent.ambulance.count";
+    public static final String KEY_AMBULANCE_NAME = "comlib.adk.tactics.ambulance.name";
+    public static final String KEY_AMBULANCE_COUNT = "comlib.adk.tactics.ambulance.count";
     
-    public static final String KEY_FIRE_NAME = "comlib.adk.agent.fire.name";
-    public static final String KEY_FIRE_COUNT = "comlib.adk.agent.fire.count";
+    public static final String KEY_FIRE_NAME = "comlib.adk.tactics.fire.name";
+    public static final String KEY_FIRE_COUNT = "comlib.adk.tactics.fire.count";
     
-    public static final String KEY_POLICE_NAME = "comlib.adk.agent.police.name";
-    public static final String KEY_POLICE_COUNT = "comlib.adk.agent.police.count";
+    public static final String KEY_POLICE_NAME = "comlib.adk.tactics.police.name";
+    public static final String KEY_POLICE_COUNT = "comlib.adk.tactics.police.count";
 }
