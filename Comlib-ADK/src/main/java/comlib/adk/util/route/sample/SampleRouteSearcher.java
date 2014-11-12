6e793fddee5f81477489e13b63f37ec1824e8d18
uranos <uranoskoro@gmail.com>
2014-11-12 23:02:31 +0900

    fix

--- Comlib-ADK/src/main/java/comlib/adk/util/route/sample/SampleRouteSearcher.java
+++ Comlib-ADK/src/main/java/comlib/adk/util/route/sample/SampleRouteSearcher.java
@@ -44,7 +44,7 @@ public class SampleRouteSearcher extends RouteSearcher {
     }
 
     @Override
-    public List<EntityID> noTargetWalk() {
+    public List<EntityID> noTargetWalk(int time) {
         List<EntityID> result = new ArrayList<>(RANDOM_WALK_LENGTH);
         Set<EntityID> seen = new HashSet<>();
         EntityID current = ((Human)this.tactics.me()).getPosition();
--- Comlib-ADK/src/main/java/comlib/adk/util/route/serversample/ServerSampleRouteSearcher.java
+++ Comlib-ADK/src/main/java/comlib/adk/util/route/serversample/ServerSampleRouteSearcher.java
@@ -43,7 +43,7 @@ public class ServerSampleRouteSearcher extends RouteSearcher{
     }
 
     @Override
-    public List<EntityID> noTargetWalk() {
+    public List<EntityID> noTargetWalk(int time) {
         List<EntityID> result = new ArrayList<>(RANDOM_WALK_LENGTH);
         Set<EntityID> seen = new HashSet<>();
         EntityID current = ((Human) this.tactics.me()).getPosition();
