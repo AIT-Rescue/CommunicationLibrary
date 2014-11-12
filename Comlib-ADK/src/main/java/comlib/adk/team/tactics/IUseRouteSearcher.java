f76c3d53a0de57a550d54c2ab978fa046357bb97
uranos <uranoskoro@gmail.com>
2014-11-13 02:31:14 +0900

    add interface

---
+++ Comlib-ADK/src/main/java/comlib/adk/team/tactics/IUseRouteSearcher.java
@@ -0,0 +1,10 @@
+package comlib.adk.team.tactics;
+
+import comlib.adk.util.route.IRouteSearcher;
+
+public interface IUseRouteSearcher {
+
+    public void initRouteSearcher();
+
+    public IRouteSearcher getRouteSearcher();
+}
